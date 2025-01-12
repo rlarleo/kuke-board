74fef510abb18db081df32f265dd0cb3a7444ae4
rlarleo <soulen2403@naver.com>
2025-01-12 12:34:32 +0900

    :tada: init

---
+++ service/article/src/main/java/kuke/board/article/AricleApplication.java
@@ -0,0 +1,11 @@
+package kuke.board.article;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AricleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AricleApplication.class, args);
+    }
+}
